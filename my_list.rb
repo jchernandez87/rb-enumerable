69c8f5b48bd8efddd0ca30736c1add85161a4616
AlexRS90 <aramosbengy@gmail.com>
2022-01-20 14:39:19 -0600

    Add test cases for all method and import module

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,6 @@
 class MyList
+  require "./my_enumerable.rb"
+  include MyEnumerable
   def initialize (arr)
     @list = arr
   end
@@ -9,3 +11,6 @@ class MyList
 end
 
 my_list = MyList.new([1,2,3,4])
+
+puts my_list.all? {|e| e < 5}
+puts my_list.all? {|e| e > 5}
